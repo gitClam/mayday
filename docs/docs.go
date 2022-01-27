2134e38b521166d97e5827a7eed63a6736ebddd6
li rong cheng <lirongcheng@aobi.com>
2022-01-27 17:39:10 +0800

    swagger 配置文件生成

--- docs/docs.go
+++ docs/docs.go
@@ -50,7 +50,7 @@ var SwaggerInfo = swaggerInfo{
 	BasePath:    "/",
 	Schemes:     []string{},
 	Title:       "JieTong API",
-	Description: "除了用户登录和注册以及头像获取三个接口\n其他的都需要用户携带TOKEN进行用户验证，否则无法访问接口\n",
+	Description: "除了用户登录和注册以及头像获取三个接口\n其他的都需要用户携带TOKEN进行用户验证，否则无法访问接口\n\nTOKEN 格式 ： KEY：Authorization VALUE： \"JWT \" + 登录时返回的对应TOKEN   （放在请求的header中）\n",
 }
 
 type s struct{}
