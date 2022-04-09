0b0650f277cddf9fdb773a682632cf63bf1fafad
791285634 <791285634@qq.com>
2022-04-09 16:20:28 +0800

    for bug

--- src/service/workflow/workflow.go
+++ src/service/workflow/workflow.go
@@ -2,11 +2,13 @@ package workflowService
 
 import (
 	"github.com/kataras/iris/v12"
+	"go.uber.org/zap"
 	"mayday/src/global"
 	"mayday/src/model/common/resultcode"
 	"mayday/src/model/common/timedecoder"
 	UserModel "mayday/src/model/user"
 	WorkflowModel "mayday/src/model/workflow"
+	"mayday/src/model/workspace/application"
 	"mayday/src/utils"
 	"time"
 )
@@ -20,40 +22,31 @@ func CreateWorkflow(ctx iris.Context, workflowReq WorkflowModel.WorkflowReq) {
 	sdWorkflow.CreateTime = timedecoder.LocalTime(time.Now())
 	sdWorkflow.CreateUser = user.Id
 
-	//方便测试
-	e := global.GVA_DB
-	effect, err := e.Insert(sdWorkflow)
+	//正确写法
+	session := global.GVA_DB.NewSession()
+	effect, err := session.Insert(&sdWorkflow)
 	if effect <= 0 || err != nil {
 		utils.Responser.Fail(ctx, resultcode.DataCreateFail, err)
+		if err := session.Rollback(); err != nil {
+			global.GVA_LOG.Error("数据库事务回滚失败", zap.Error(err))
+		}
+		return
+	}
+	SdWorkflowApplication := application.SdWorkflowApplication{WorkflowId: sdWorkflow.Id, ApplicationId: workflowReq.ApplicationId}
+	effect, err = session.Insert(SdWorkflowApplication)
+	if effect <= 0 || err != nil {
+		utils.Responser.Fail(ctx, resultcode.DataCreateFail, err)
+		if err := session.Rollback(); err != nil {
+			global.GVA_LOG.Error("数据库事务回滚失败", zap.Error(err))
+		}
 		return
 	}
 
-	//正确写法
-	//session := e.NewSession()
-	//
-	//effect, err := session.Insert(&sdWorkflow)
-	//if effect <= 0 || err != nil {
-	//	utils.Responser.FailWithMsg(ctx, "流程创建失败", err)
-	//	if err := session.Rollback(); err != nil {
-	//		global.GVA_LOG.Error("数据库事务回滚失败", zap.Error(err))
-	//	}
-	//	return
-	//}
-	//SdWorkflowApplication := model.SdWorkflowApplication{WorkflowId: sdWorkflow.Id, ApplicationId: workflowReq.ApplicationId}
-	//effect, err := session.Insert(SdWorkflowApplication)
-	//if effect <= 0 || err != nil {
-	//	utils.Responser.FailWithMsg(ctx, "流程创建失败", err)
-	//	if err := session.Rollback(); err != nil {
-	//		global.GVA_LOG.Error("数据库事务回滚失败", zap.Error(err))
-	//	}
-	//	return
-	//}
-	//
-	//if err := session.Commit(); err != nil {
-	//	utils.Responser.FailWithMsg(ctx, "流程创建失败", err)
-	//	global.GVA_LOG.Error("数据库事务提交失败", zap.Error(err))
-	//	return
-	//}
+	if err := session.Commit(); err != nil {
+		utils.Responser.Fail(ctx, resultcode.DataCreateFail, err)
+		global.GVA_LOG.Error("数据库事务提交失败", zap.Error(err))
+		return
+	}
 
 	utils.Responser.Ok(ctx)
 }
