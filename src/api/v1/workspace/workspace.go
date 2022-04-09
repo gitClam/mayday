c1018e4c1bc537b68467c146ff4a1f42dcdbdff7
791285634 <791285634@qq.com>
2022-04-10 00:53:52 +0800

    for bug

--- src/api/v1/workspace/workspace.go
+++ src/api/v1/workspace/workspace.go
@@ -238,7 +238,11 @@ func WorkspaceSelectUseByWorkspaceId(ctx iris.Context) {
 	var allUser []userModel.SdUser
 	for _, workspaceId := range workspaceIds {
 		var users []userModel.SdUser
-		global.GVA_DB.SQL("select * from sd_user where id IN (SELECT user_id FROM sd_user_job WHERE job_id IN (SELECT id FROM sd_job  WHERE department_id IN (SELECT id FROM sd_department WHERE workspace_id = ？)))", workspaceId).Find(&users)
+		err := global.GVA_DB.SQL("select * from sd_user where id IN (SELECT user_id FROM sd_user_job WHERE job_id IN (SELECT id FROM sd_job  WHERE department_id IN (SELECT id FROM sd_department WHERE workspace_id = ？)))", workspaceId).Find(&users)
+		if err != nil {
+			utils.Responser.Fail(ctx, resultcode.DataSelectFail, err)
+			return
+		}
 		allUser = append(allUser, users...)
 	}
 	utils.Responser.OkWithDetails(ctx, allUser)
