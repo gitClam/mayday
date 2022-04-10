07b4fb556711867f9cf1ef7a7832dd86cd2b639f
791285634 <791285634@qq.com>
2022-04-10 21:07:56 +0800

    for bug

--- src/service/order/create.go
+++ src/service/order/create.go
@@ -49,7 +49,7 @@ func CreateOrderService(ctx iris.Context, user *user.SdUser) (err error) {
 		//}
 	)
 	//获取请求的全部数据
-	err = ctx.ReadJSON(&workOrderValue)
+	err = ctx.ReadForm(&workOrderValue)
 	if err != nil {
 		global.GVA_LOG.Warn("数据接收失败", zap.Error(err))
 		return
